c40d84ea032b93763b07686fa1836df940ec4dd4
Andrew Kamble <andrewkamble88@gmail.com>
2019-07-12 13:29:20 +0530

    Update beidou_b1c_signal_processing_new.h

--- src/algorithms/libs/beidou_b1c_signal_processing_new.h
+++ src/algorithms/libs/beidou_b1c_signal_processing_new.h
@@ -1,3 +1,36 @@
+/*!
+ * \file beidou_b1c_signal_processing_new.h
+ * \brief This class implements signal generators for the BeiDou B1c signals
+ * \author Andrew Kamble, 2019. andrewkamble88@gmail.com 
+ * \note Code added as part of GSoC 2019 program
+ * Detailed description of the file here if needed.
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2019 (see AUTHORS file for a list of contributors)
+ *
+ * GNSS-SDR is a software defined Global Navigation
+ *          Satellite Systems receiver
+ *
+ * This file is part of GNSS-SDR.
+ *
+ * GNSS-SDR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GNSS-SDR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNSS-SDR. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+
 #ifndef GNSS_SDR_BEIDOU_B1C_SIGNAL_PROCESSING_H_
 #define GNSS_SDR_BEIDOU_B1C_SIGNAL_PROCESSING_H_
 
