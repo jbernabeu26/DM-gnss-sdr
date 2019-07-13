31d8a7bebc228848638b4d8148b769d3aaec375f
Andrew Kamble <andrewkamble88@gmail.com>
2019-07-13 22:48:17 +0530

    Changed with new "gsl" settings

--- src/algorithms/libs/beidou_b1c_signal_processing.h
+++ src/algorithms/libs/beidou_b1c_signal_processing.h
@@ -44,37 +44,40 @@ namespace gsl = std;
 //! Generates BeiDou B1c Data Primary codes for the desired SV ID
 void make_b1cd(gsl::span<int32_t> _dest, int32_t prn);
 
-//! Generates a float version of BeiDou B1c Data Primary code for the desired SV ID
-void beidou_b1cd_code_gen_float(float* _dest, unsigned int _prn);
+//! Generates a float version of BeiDou B1C Data Primary code for the desired SV ID
+void beidou_b1cd_code_gen_float(gsl::span<float> _dest, uint32_t _prn);
 
-//! Generates a complex version of BeiDou B1c Data Primary code for the desired SV ID
-void beidou_b1cd_code_gen_complex(std::complex<float>* _dest, unsigned int _prn);
+//! Generate a complex version of the B1C Data Primary code
+void beidou_b1cd_code_gen_complex(gsl::span<std::complex<float>> _dest, uint32_t _prn);
 
-//! Generates complex BEIDOU B1c Data Primary code for the desired SV ID and sampled to specific sampling frequency
-void beidou_b1cd_code_gen_complex_sampled(std::complex<float>* _dest, unsigned int _prn, signed int _fs);
+//! Generates complex BEIDOU B1C Data Primary code for the desired SV ID and sampled to specific sampling frequency
+void beidou_b1cd_code_gen_complex_sampled(gsl::span<std::complex<float>> _dest, uint32_t _prn, int32_t _fs);
 
-//! Generates the BeiDou B1c Pilot Primary code
-void make_b1cp(int32_t* _dest, int prn);
+//! Generate the B1C Pilot Primary code
+void make_b1cp(gsl::span<int32_t> _dest, int32_t prn);
 
-//! Generates a float version of BeiDou B1c Pilot Primary code
-void beidou_b1cp_code_gen_float(float* _dest, unsigned int _prn);
+//! Generate a float version of the B1C Pilot Primary code
+void beidou_b1cp_code_gen_float(gsl::span<float> _dest, uint32_t _prn);
 
-//! Generates a complex version of BeiDou B1c Pilot Primary code
-void beidou_b1cp_code_gen_complex(std::complex<float>* _dest, unsigned int _prn);
+//! Generates a complex version of BeiDou B1C Pilot Primary code
+void beidou_b1cp_code_gen_complex(gsl::span<std::complex<float>> _dest, uint32_t _prn);
 
-//! Generates complex BEIDOU B1c Pilot Primary code for the desired SV ID and sampled to specific sampling frequency
-void beidou_b1cp_code_gen_complex_sampled(std::complex<float>* _dest, unsigned int _prn, signed int _fs);
+//! Generates complex BEIDOU B1C Pilot Primary code for the desired SV ID and sampled to specific sampling frequency
+void beidou_b1cp_code_gen_complex_sampled(gsl::span<std::complex<float>> _dest, uint32_t _prn, int32_t _fs);
 
-//! Generates a version of the B1c Pilot Secondary code
-void make_b1cp_secondary(int32_t* _dest, int prn);
+//! Generate a version of the B1C Pilot code with the secondary pilot code included
+void make_b1cp_secondary(gsl::span<int32_t> _dest, int32_t prn);
 
-//! Generates a complex version of the B1c Pilot Secondary code
-void beidou_b1cp_code_gen_complex_secondary(std::complex<float>* _dest, unsigned int _prn);
+//! Generates a float version of the B1C pilot primary code Note:Newly Added
+void beidou_b1cp_code_gen_float_secondary(gsl::span<float> _dest, uint32_t _prn);
 
-//! Generates complex BEIDOU B1c Pilot Secondary code for the desired SV ID and sampled to specific sampling frequency
-void beidou_b1cp_code_gen_complex_sampled_secondary(std::complex<float>* _dest, unsigned int _prn, signed int _fs);
+//! Generate a complex version of the B1C pilot code with the secondary pilot code
+void beidou_b1cp_code_gen_complex_secondary(gsl::span<std::complex<float>> _dest, uint32_t _prn);
+
+//! Generates complex BEIDOU B1C pilot code for the desired SV ID and sampled to specific sampling frequency with the secondary code implemented
+void beidou_b1cp_code_gen_complex_sampled_secondary(gsl::span<std::complex<float>> _dest, uint32_t _prn, int32_t _fs);
 
 //! Generates complex BEIDOU B1C data+pilot code for the desired SV ID and sampled to specific sampling frequency
-void beidou_b1c_code_gen_complex_sampled(std::complex<float>* _dest, uint32_t _prn, int32_t _fs);
+void beidou_b1c_code_gen_complex_sampled(gsl::span<std::complex<float>> _dest, uint32_t _prn, int32_t _fs);
 
 #endif /* GNSS_SDR_BEIDOU_B1C_SIGNAL_PROCESSING_H_ */
